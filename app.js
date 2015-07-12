b11ef52d69b8c40ea1b3b74134508f278cd2a602
Leto <leto@kauler.com>
2015-07-12 21:48:24 +1000

    Fleshing out list/detail generic views using templates

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ function DndTools() {
 
 	// Data
 
+	var encounters = [
+		new Encounter(1, "Encounter 1"),
+		new Encounter(2, "Encounter 2"),
+		new Encounter(3, "Encounter 3"),
+		new Encounter(4, "Encounter 4"),
+		new Encounter(5, "Encounter 5")
+	];
+
 	self.selectedPage = ko.observable();
 	self.pages = [
-		new Page("campaign", "Campaign", "language", false),
-		new Page("encounter", "Encounters", "warning", true),
-		new Page("monster", "Monster templates", "adb", false)
+		new Page("campaign", "Campaign", "language", false, []),
+		new Page("encounter", "Encounters", "warning", true, encounters),
+		new Page("monster", "Monster templates", "adb", false, [])
 	];
 
 	// Behaviour
 
+	self.selectPageObject = function(obj) {
+		self.selectedPage().selectedObject(obj);
+	}
+
 	self.goToPage = function(page) {
+		// Reset
+		self.addButtonClick(false);
+
 		// Find ID in collection
 		for (var i = 0; i < self.pages.length; i++) {
 			if (page && page.id === self.pages[i].id) {
@@ -23,12 +38,16 @@ function DndTools() {
 			}
 		};
 		// Not found
-		self.selectedPage(self.pages[0]);
+		self.selectedPage(self.pages[1]);
 	}
 
+	self.addButtonClick = ko.observable(false);
 	self.showAddButton = ko.pureComputed(function() {
 		return this.selectedPage().allowsAdd === true;
 	}, self);
+	self.clickAddButton = function() {
+		self.addButtonClick(true);
+	}
 
 	// Init
 
@@ -39,16 +58,25 @@ function DndTools() {
 
 // Models
 
-function Page(id, title, icon, allowsAdd) {
+function Page(id, title, icon, allowsAdd, objects) {
 	this.id = id;
 	this.title = title;
 	this.icon = icon;
 	this.allowsAdd = allowsAdd;
+	this.objects = objects;
+
+	this.templateName = function() {
+		var name = this.id + '-template';
+		return (document.getElementById(name) == null) ? '' : name;
+	}
+
+	this.selectedObject = ko.observable();
 }
 
-function Encounter(id, title) {
+function Encounter(id, title, notes) {
 	this.id = id;
-	this.title = title;
+	this.title = title || 'Untitled';
+	this.notes = notes || '';
 }
 
 // Init
